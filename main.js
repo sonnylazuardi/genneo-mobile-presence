092f0fda87240b8ae3a57b96ef41e0fbd8e487de
Sonny Lazuardi <sonnylazuardi@gmail.com>
2017-03-02 15:47:14 +0700

    reselect

--- main.js
+++ main.js
@@ -1,83 +1,186 @@
 import Exponent from 'exponent';
 import React from 'react';
 import {
-  AppRegistry,
-  Platform,
-  StatusBar,
-  StyleSheet,
-  View,
+    AppRegistry,
+    Platform,
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native';
-import {
-  NavigationProvider,
-  StackNavigation,
-} from '@exponent/ex-navigation';
-import {
-  FontAwesome,
-} from '@exponent/vector-icons';
+import {createStore, applyMiddleware, combineReducers, bindActionCreators} from 'redux';
+import {connect, Provider} from 'react-redux';
+import thunk from 'redux-thunk'; 
+import { createSelector } from 'reselect';
 
-import Router from './navigation/Router';
-import cacheAssetsAsync from './utilities/cacheAssetsAsync';
+const SERVER_URL = `http://api.sonnylab.com/genneo`;
 
-class AppContainer extends React.Component {
-  state = {
-    appIsReady: false,
-  }
-
-  componentWillMount() {
-    this._loadAssetsAsync();
-  }
-
-  async _loadAssetsAsync() {
-    try {
-      await cacheAssetsAsync({
-        images: [
-          require('./assets/images/exponent-wordmark.png'),
-        ],
-        fonts: [
-          FontAwesome.font,
-          {'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')},
-        ],
-      });
-    } catch(e) {
-      console.warn(
-        'There was an error caching assets (see: main.js), perhaps due to a ' +
-        'network timeout, so we skipped caching. Reload the app to try again.'
-      );
-      console.log(e.message);
-    } finally {
-      this.setState({appIsReady: true});
+const initialPendaftar = {
+    pendaftar: [],
+    hadir: []
+};
+const pendaftarReducer = (state = initialPendaftar, action = {}) => {
+    const {payload} = action;
+    switch(action.type) {
+        case 'RECEIVE_PENDAFTAR':
+            return {...state, pendaftar: payload.pendaftar}
+        case 'RECEIVE_HADIR':
+            return {...state, hadir: payload.hadir}
+        default:
+            return state;
     }
-  }
+};
 
-  render() {
-    if (this.state.appIsReady) {
-      return (
-        <View style={styles.container}>
-          <NavigationProvider router={Router}>
-            <StackNavigation id="root" initialRoute={Router.getRoute('rootNavigation')} />
-          </NavigationProvider>
-
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
-        </View>
-      );
-    } else {
-      return (
-        <Exponent.Components.AppLoading />
-      );
+const api = {
+    get: (url, options) => {
+        return fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json()).then(data => {
+            return data
+        }).catch(err => console.log(err));
+    },
+    post: (url, params, options) => {
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        }).then(response => response.json()).then(data => {
+            return data;
+        }).catch(err => console.log(err));
     }
-  }
+}
+
+//actions
+const receivePendaftar = (pendaftar) => {
+    return {
+        type: 'RECEIVE_PENDAFTAR',
+        payload: {
+            pendaftar
+        }
+    }
+};
+
+const fetchPendaftar = () => {
+    return (dispatch, getState) => {
+        return api.get(`${SERVER_URL}/listpendaftar`).then(data => {
+            dispatch(receivePendaftar(data));
+            return data;
+        });
+    }
+};
+
+const addHadir = (pendaftar) => {
+    return (dispatch, getState) => {
+        return api.post(`${SERVER_URL}/presences`, {email: pendaftar.email}).then(data => {
+            dispatch(receiveHadir(data));
+            return data;
+        });
+    }
+} 
+
+const receiveHadir = (hadir) => {
+    return {
+        type: 'RECEIVE_HADIR',
+        payload: {
+            hadir
+        }
+    }
+};
+
+const fetchHadir = () => {
+    return (dispatch, getState) => {
+        return api.get(`${SERVER_URL}/presences`).then(data => {
+            dispatch(receiveHadir(data));
+            return data;
+        });
+    }
+};
+
+//selectros
+const pendaftarSelector = state => state.pendaftar.pendaftar;
+const hadirSelector = state => state.pendaftar.hadir;
+
+const getHadir = (pendaftar, hadir) => {
+    const pendaftarHadir = pendaftar.map(item => ({
+        ...item, 
+        hadir: (hadir.indexOf(item.email) !== -1)
+    }));
+    return pendaftarHadir;
+};
+
+const pendaftarHadirSelector = createSelector(
+    pendaftarSelector,
+    hadirSelector,
+    getHadir
+);
+
+const reducers = combineReducers({
+    pendaftar: pendaftarReducer
+});
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk)
+);
+
+class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Pendaftar />  
+            </Provider>
+        );
+    }
+}
+
+@connect(state => ({
+    pendaftar: state.pendaftar,
+    pendaftarHadir: pendaftarHadirSelector(state)
+}))
+class Pendaftar extends React.Component {
+    componentDidMount() {
+        // load list pendaftar
+        this.props.dispatch(fetchHadir()).then(() => {
+            this.props.dispatch(fetchPendaftar());
+        });
+    }
+    onHadir = (pendaftar) => {
+        this.props.dispatch(addHadir(pendaftar));
+    };
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+                    {this.props.pendaftarHadir.map(pendaftar => {
+                        return (
+                            <View style={{paddingVertical: 10, borderWidth: 1, borderColor: '#eee'}}>
+                                <Text>{pendaftar.nama}</Text>
+                                <Text>{pendaftar.email}</Text>
+                                <TouchableOpacity onPress={() => this.onHadir(pendaftar)}>
+                                    {pendaftar.hadir ?
+                                        <Text>Hadir</Text>
+                                        : <Text>Tidak</Text>}
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    })}
+                </ScrollView>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
+    container: {
+        flex: 1
+    },
 });
 
-Exponent.registerRootComponent(AppContainer);
+Exponent.registerRootComponent(App);
